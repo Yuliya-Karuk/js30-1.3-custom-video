e2751543aa173fb8d2123cbb5356dd92108f3f45
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-06-15 00:07:06 +0400

    fix: add functions which show progress when you watch the video, and function which show changing the volume of video

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* SELECTORS */
 const player = document.querySelector('.video');
-const video = player.querySelector('.main-video'); //для чего
+const video = player.querySelector('.main-video');
 
 // controls
 const controlPlay = player.querySelector('.play-btn');
@@ -10,12 +10,7 @@ const buttonVolume = player.querySelector('.volume-btn');
 const svgVolume = player.querySelector('.volume-svg');
 const svgMute = player.querySelector('.mute-svg');
 const controlVolume = player.querySelector('.volume');
-
-// const controlRate = vidWrapper.querySelector('.player__slider[name="playbackRate"]');
-// const controlSkip = vidWrapper.querySelectorAll('.player__button[data-skip]');
-// const controlFullScreen = vidWrapper.querySelector('.player__fullscreen');
-// const controlProgress = vidWrapper.querySelector('.progress');
-// const progressBar = vidWrapper.querySelector('.progress__filled');
+const controlTimer = player.querySelector('.timer');
 
 function togglePlay() {
     if (video.paused) {
@@ -29,12 +24,23 @@ function togglePlay() {
     }
 }
 
-function updateVolume() {
+// функция которая изменяет ползунок звука
+function changeVolume() { 
+    if (video.muted === true) { 
+        toggleVolume()
+    }
+
     let volume = this.value;
     video.volume = volume;
+    this.style.background = `linear-gradient(to right, #bdae82  0%, #bdae82  ${volume * 100}%, #c8c8c8 ${volume * 100}%, #c8c8c8 100%)`
+
+    if (video.volume === 0) {
+        video.muted = false
+        toggleVolume()
+    }
 }
 
-
+// функция которая выключает/включает звук и меняет при этом кнопку
 function toggleVolume() {
     if (!video.muted) {
         video.muted = true
@@ -47,8 +53,23 @@ function toggleVolume() {
     }
 }
 
+// функция которая показывает изменение времени на ползунке времени
+function handlerTimer() {
+    const percent = (video.currentTime / video.duration) * 100;
+    controlTimer.style.background = `linear-gradient(to right, #bdae82  0%, #bdae82  ${percent}%, #c8c8c8 ${percent}%, #c8c8c8 100%)`
+    controlTimer.value = percent
+}
+
+// функция, которая при клике на ползунке времени изменяет время 
+function moveTimer() {
+    const time = this.value;
+    const progress = time * video.duration / 100
+    video.currentTime = progress
+}
+
 video.addEventListener('click', togglePlay) // play и pause при клике на видео
 controlPlay.addEventListener('click', togglePlay) // play и pause при клике на кнопку Play/Pause
-
 buttonVolume.addEventListener('click', toggleVolume) // mute и unmute звук при клике на кнопку звука
-
+controlVolume.addEventListener('input', changeVolume) // изменение звука на ползунке
+video.addEventListener('timeupdate', handlerTimer) // изменение ползунка времени при просмотре видео
+controlTimer.addEventListener('input', moveTimer) // изменение времени при клике на ползунок времени
