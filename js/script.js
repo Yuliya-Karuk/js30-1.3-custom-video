d9e72e62ab7492353ebbf1970ac5348d4c12a9db
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-06-15 00:15:55 +0400

    refactor: add semicolon in JS file, change some hex color on var

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ const controlTimer = player.querySelector('.timer');
 
 function togglePlay() {
     if (video.paused) {
-        video.play()
+        video.play();
         svgPlay.classList.add('visually-hidden');
         svgPause.classList.remove('visually-hidden');
     } else {
-        video.pause()
+        video.pause();
         svgPlay.classList.remove('visually-hidden');
         svgPause.classList.add('visually-hidden');
     }
@@ -27,27 +27,27 @@ function togglePlay() {
 // функция которая изменяет ползунок звука
 function changeVolume() { 
     if (video.muted === true) { 
-        toggleVolume()
+        toggleVolume();
     }
 
     let volume = this.value;
     video.volume = volume;
-    this.style.background = `linear-gradient(to right, #bdae82  0%, #bdae82  ${volume * 100}%, #c8c8c8 ${volume * 100}%, #c8c8c8 100%)`
+    this.style.background = `linear-gradient(to right, var(--basic-yellow)  0%, var(--basic-yellow)  ${volume * 100}%, var(--basic-gray) ${volume * 100}%, var(--basic-gray) 100%)`;
 
     if (video.volume === 0) {
-        video.muted = false
-        toggleVolume()
+        video.muted = false;
+        toggleVolume();
     }
 }
 
 // функция которая выключает/включает звук и меняет при этом кнопку
 function toggleVolume() {
     if (!video.muted) {
-        video.muted = true
+        video.muted = true;
         svgVolume.classList.add('visually-hidden');
         svgMute.classList.remove('visually-hidden');
     } else {
-        video.muted = false
+        video.muted = false;
         svgMute.classList.add('visually-hidden');
         svgVolume.classList.remove('visually-hidden');
     }
@@ -56,20 +56,20 @@ function toggleVolume() {
 // функция которая показывает изменение времени на ползунке времени
 function handlerTimer() {
     const percent = (video.currentTime / video.duration) * 100;
-    controlTimer.style.background = `linear-gradient(to right, #bdae82  0%, #bdae82  ${percent}%, #c8c8c8 ${percent}%, #c8c8c8 100%)`
-    controlTimer.value = percent
+    controlTimer.style.background = `linear-gradient(to right, var(--basic-yellow)  0%, var(--basic-yellow)  ${percent}%, var(--basic-gray) ${percent}%, var(--basic-gray) 100%)`;
+    controlTimer.value = percent;
 }
 
 // функция, которая при клике на ползунке времени изменяет время 
 function moveTimer() {
     const time = this.value;
-    const progress = time * video.duration / 100
-    video.currentTime = progress
+    const progress = time * video.duration / 100;
+    video.currentTime = progress;
 }
 
-video.addEventListener('click', togglePlay) // play и pause при клике на видео
-controlPlay.addEventListener('click', togglePlay) // play и pause при клике на кнопку Play/Pause
-buttonVolume.addEventListener('click', toggleVolume) // mute и unmute звук при клике на кнопку звука
-controlVolume.addEventListener('input', changeVolume) // изменение звука на ползунке
-video.addEventListener('timeupdate', handlerTimer) // изменение ползунка времени при просмотре видео
-controlTimer.addEventListener('input', moveTimer) // изменение времени при клике на ползунок времени
+video.addEventListener('click', togglePlay); // play и pause при клике на видео
+controlPlay.addEventListener('click', togglePlay); // play и pause при клике на кнопку Play/Pause
+buttonVolume.addEventListener('click', toggleVolume); // mute и unmute звук при клике на кнопку звука
+controlVolume.addEventListener('input', changeVolume); // изменение звука на ползунке
+video.addEventListener('timeupdate', handlerTimer); // изменение ползунка времени при просмотре видео
+controlTimer.addEventListener('input', moveTimer); // изменение времени при клике на ползунок времени
